eba1e7e01b780cf0dc38d05d84c8d7ecf96ce37a
Jon Grythe Stødle <jon@stodle.net>
2026-01-18 22:47:54 +0100

    Print progress

--- Skvis/Program.cs
+++ Skvis/Program.cs
@@ -4,18 +4,25 @@ using SixLabors.ImageSharp.Formats.Webp;
 string[] imageExtensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff"];
 var sourcePath = args[0];
 
-var filePaths = Directory.Exists(sourcePath) ? Directory.GetFiles(sourcePath)
-    : File.Exists(sourcePath) ? [sourcePath]
-    : throw new DirectoryNotFoundException($"Could not find any file or directory at '{sourcePath}'");
+var filePaths = (Directory.Exists(sourcePath) ? Directory.GetFiles(sourcePath)
+        : File.Exists(sourcePath) ? [sourcePath]
+        : throw new DirectoryNotFoundException($"Could not find any file or directory at '{sourcePath}'"))
+    .Where(fp => imageExtensions.Contains(Path.GetExtension(fp).ToLower()))
+    .ToList();
+
+var imagesDone = 0;
 
 filePaths
-    .Where(fp => imageExtensions.Contains(Path.GetExtension(fp).ToLower()))
     .AsParallel()
     .WithDegreeOfParallelism(3)
     .ForAll(fp =>
     {
         var originalPath = Path.GetFullPath(fp);
         var destinationPath = Path.ChangeExtension(fp, "webp");
+        
         Image.Load(originalPath)
             .SaveAsWebp(destinationPath, new WebpEncoder { Quality = 70 });
+        
+        var numberDone = Interlocked.Increment(ref imagesDone);
+        Console.WriteLine($"Conversion done: {Path.GetFileName(destinationPath)} [{numberDone}/{filePaths.Count}]");
     });
