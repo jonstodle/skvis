96b8f80caf452cb21d3a16ada6af3a88c24db780
Jon Grythe Stødle <jon@stodle.net>
2026-01-18 22:24:17 +0100

    Add image conversion

--- Skvis/Program.cs
+++ Skvis/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Webp;
 
-Console.WriteLine("Hello, World!");
+string[] imageExtensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff"];
+var sourcePath = args[0];
+
+var filePaths = Directory.Exists(sourcePath) ? Directory.GetFiles(sourcePath)
+    : File.Exists(sourcePath) ? [sourcePath]
+    : throw new DirectoryNotFoundException($"Could not find any file or directory at '{sourcePath}'");
+
+filePaths
+    .Where(fp => imageExtensions.Contains(Path.GetExtension(fp).ToLower()))
+    .AsParallel()
+    .WithDegreeOfParallelism(3)
+    .ForAll(fp =>
+    {
+        var originalPath = Path.GetFullPath(fp);
+        var destinationPath = Path.ChangeExtension(fp, "webp");
+        Image.Load(originalPath)
+            .SaveAsWebp(destinationPath, new WebpEncoder { Quality = 70 });
+    });
