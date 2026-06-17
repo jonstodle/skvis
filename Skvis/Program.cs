d2d657534123a755774c6b3f6948dfc3e30ed071
Jon Grythe Stødle <jon@stodle.net>
2026-06-17 16:40:33 +0200

    Add quality and resolution options

--- Skvis/Program.cs
+++ Skvis/Program.cs
@@ -1,29 +1,62 @@
-﻿using SixLabors.ImageSharp;
+﻿using System.CommandLine;
+using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Webp;
+using SixLabors.ImageSharp.Processing;
 
 var imageExtensions = new HashSet<string> { ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff" };
 
-var filePaths = args
-    .SelectMany(sourcePath => Directory.Exists(sourcePath) ? Directory.GetFiles(sourcePath)
-        : File.Exists(sourcePath) ? [sourcePath]
-        : throw new DirectoryNotFoundException($"Could not find any file or directory at '{sourcePath}'"))
-    .Where(fp => imageExtensions.Contains(Path.GetExtension(fp).ToLower()))
-    .ToList();
+RootCommand app = new("Skvis");
 
-var imagesDone = 0;
+Option<int> qualityOption = new("--quality", "-q") { Description = "Output quality", DefaultValueFactory = _ => 70 };
+app.Options.Add(qualityOption);
 
-filePaths
-    .AsParallel()
-    .WithDegreeOfParallelism(3)
-    .ForAll(fp =>
-    {
-        var originalPath = Path.GetFullPath(fp);
-        var destinationPath = Path.ChangeExtension(fp, "webp");
+Option<int> maxBoundOption = new("--max", "-x") { Description = "Max width and height", DefaultValueFactory = _ => 0 };
+app.Options.Add(maxBoundOption);
 
-        Image.Load(originalPath)
-            .SaveAsWebp(destinationPath, new WebpEncoder { Quality = 70 });
+Argument<List<FileSystemInfo>> entriesArgument = new("paths")
+{
+    Description = "Paths to image files or directories containing image files",
+    Arity = ArgumentArity.OneOrMore
+};
+app.Arguments.Add(entriesArgument);
 
-        var numberDone = Interlocked.Increment(ref imagesDone);
-        Console.WriteLine(
-            $"[{numberDone.ToString().PadLeft(filePaths.Count.ToString().Length)}/{filePaths.Count}] done: {Path.GetFileName(destinationPath)}");
-    });
+app.SetAction(parseResult =>
+{
+    var filePaths = parseResult.GetRequiredValue(entriesArgument)
+        .SelectMany(entry => Directory.Exists(entry.FullName) ? Directory.GetFiles(entry.FullName)
+            : File.Exists(entry.FullName) ? [entry.FullName]
+            : throw new DirectoryNotFoundException($"Could not find any file or directory at '{entry}'"))
+        .Where(fp => imageExtensions.Contains(Path.GetExtension(fp).ToLower()))
+        .ToList();
+
+    var imagesDone = 0;
+
+    filePaths
+        .AsParallel()
+        .WithDegreeOfParallelism(3)
+        .ForAll(fp =>
+        {
+            var originalPath = Path.GetFullPath(fp);
+            var destinationPath = Path.ChangeExtension(fp, "webp");
+
+            var image = Image.Load(originalPath);
+
+            if (parseResult.GetValue(maxBoundOption) is var maxBound && maxBound > 0)
+            {
+                var newSize = image.Width > image.Height
+                    ? new Size(maxBound, 0)
+                    : new Size(0, maxBound);
+                image.Mutate(it => it.Resize(newSize));
+            }
+
+            image.SaveAsWebp(
+                destinationPath,
+                new WebpEncoder { Quality = parseResult.GetRequiredValue(qualityOption) });
+
+            var numberDone = Interlocked.Increment(ref imagesDone);
+            Console.WriteLine(
+                $"[{numberDone.ToString().PadLeft(filePaths.Count.ToString().Length)}/{filePaths.Count}] done: {Path.GetFileName(destinationPath)}");
+        });
+});
+
+app.Parse(args).Invoke();
